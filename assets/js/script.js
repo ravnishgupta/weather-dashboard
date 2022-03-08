e2f80349dec517796b81f5b87838cd66b2b5a556
Ravnish Gupta <ravnish@gmail.com>
2022-03-07 21:46:41 -0800

    retrieving and displaying current conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,15 @@ var txtCity = $("#city")
 var lblCity = $("#cityLabel");
 var geoAPI = "http://api.openweathermap.org/geo/1.0/direct";
 var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast";
-var currConditionAPI = "http://api.openweathermap.org/data/2.5/weather"
+var currConditionAPI = "http://api.openweathermap.org/data/2.5/onecall"
 var apiKey ="e3de7b451ff3cdc179f2915141cb031b";
 var lat;
 var lon;
-var tempSpan = $("#currTemp")
+var tempSpan = $("#currTemp");
+var windSpeedSpan = $("#currWind");
+var humiditySpan = $("#currHumidity");
+var uvIdxSpan = $("#currUvIndex");
+
 
 function getCityTemp(){
     event.preventDefault()
@@ -18,6 +22,7 @@ function getCityTemp(){
     }
     else {
         lblCity.text("Invalid City. Please try again");
+        resetVal();
     }   
 }
 
@@ -28,7 +33,9 @@ function getLatLong(city) {
     if (response.ok) {
       response.json().then(function(data) {
         if (Object.keys(data).length === 0) {
+          debugger;
           lblCity.text("Invalid City. Please try again");
+          resetVal();
         }
         else {
           lblCity.text(data[0].name + ", " + data[0].state + ", " + data[0].country);
@@ -47,13 +54,37 @@ function getLatLong(city) {
 }
 
 function getCurrConditions(city) {
-  var ccURL = currConditionAPI + "?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  var ccURL = currConditionAPI + "?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&exclude=hourly,daily,minutely,alerts";
   
   fetch(ccURL).then(function(response) {
     if (response.status === 200) {
       response.json().then(function(data) {
-        console.log(KelvinToFahrenheit(data.main.temp));
-        tempSpan.text(KelvinToFahrenheit(data.main.temp)+ '\u00B0 F');
+        //console.log(KelvinToFahrenheit(data.main.temp));
+        tempSpan.text(KelvinToFahrenheit(data.current.temp)+ '\u00B0 F');
+        windSpeedSpan.text(MPStoMPH(data.current.wind_speed)+ ' MPH');
+        humiditySpan.text(data.current.humidity+ '%');
+        var uvIndex = parseFloat(data.current.uvi);
+        uvIdxSpan.text(uvIndex);
+
+        uvIdxSpan.removeClass();
+        switch (true) {
+          case (uvIndex<3):
+            uvIdxSpan.addClass("rounded bg-success");
+            break;
+          case (uvIndex>=3 && uvIndex<6):
+            uvIdxSpan.addClass("rounded bgYellow")
+            break;
+          case (uvIndex>=6 && uvIndex<8):
+            uvIdxSpan.addClass("rounded bg-warning");
+            break;
+          case (uvIndex>=8 && uvIndex<11):
+            uvIdxSpan.addClass("rounded bg-danger");
+            break
+          case (uvIndex>=11):
+            uvIdxSpan.addClass("rounded bgDarkMagenta");
+            break;
+        }
+
       })
     }
     //else alert("Unable to connect to Open Weather. Please try again later.");
@@ -69,7 +100,13 @@ function KelvinToFahrenheit(temp) {
   return ((((temp-273.15)*1.8)+32).toFixed(2));
 }
 
-function speedToMPH (speed) {
-
+function MPStoMPH (speed) {
+ return ((Math.round(speed * 3600 / 1610.3*1000)/1000).toFixed(2));
 }
 
+function resetVal() {
+  tempSpan.text('');
+  windSpeedSpan.text('');
+  humiditySpan.text('');
+  uvIdxSpan.text('');
+}
